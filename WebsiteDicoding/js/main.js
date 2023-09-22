3325d02249031e003c6a2bd0998524316dda5868
Pelean Alexander Jonas Sitompul <125451797+uazaoebe@users.noreply.github.com>
2023-09-22 13:11:21 +0700

    Add files via upload

---
+++ WebsiteDicoding/js/main.js
@@ -0,0 +1,13 @@
+// Toggle & Responsive Navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+  
+    burger.addEventListener("click", () => {
+      // Toggle nav list and burger class
+      navLists.classList.toggle("nav-active");
+      burger.classList.toggle("toggle-burger");
+    });
+  };
+  
+  navSlide();
